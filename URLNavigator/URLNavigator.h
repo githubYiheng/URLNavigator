7f00320f95f949cd3731f465b4440c14d3c4f667
 王传正 <chuanzheng.wang@asiainnovations.com>
2020-05-09 15:59:03 +0800

    suport ccarthage

---
+++ URLNavigator/URLNavigator.h
@@ -0,0 +1,19 @@
+//
+//  URLNavigator.h
+//  URLNavigator
+//
+//  Created by wang2gou on 2020/5/9.
+//  Copyright © 2020 URLNavigator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for URLNavigator.
+FOUNDATION_EXPORT double URLNavigatorVersionNumber;
+
+//! Project version string for URLNavigator.
+FOUNDATION_EXPORT const unsigned char URLNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLNavigator/PublicHeader.h>
+
+
